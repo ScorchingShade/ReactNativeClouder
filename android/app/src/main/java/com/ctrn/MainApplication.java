580be43c689556f3650db3be1546eb1e2c997be0
ScorchingShade <ankushors789@gmail.com>
2020-11-23 11:41:24 +0530

    added geofence

--- android/app/src/main/java/com/ctrn/MainApplication.java
+++ android/app/src/main/java/com/ctrn/MainApplication.java
@@ -16,9 +16,18 @@ import com.clevertap.android.sdk.ActivityLifecycleCallback;
 import com.clevertap.react.CleverTapPackage;
 import com.clevertap.android.sdk.CleverTapAPI;
 
+import com.clevertap.android.geofence.CTGeofenceAPI;
+import com.clevertap.android.geofence.CTGeofenceSettings;
+import com.clevertap.android.geofence.Logger;
+import com.clevertap.android.geofence.interfaces.CTGeofenceEventsListener;
+import com.clevertap.android.geofence.interfaces.CTLocationUpdatesListener;
+import android.widget.Toast;
+import android.location.Location;
+import org.json.JSONObject;
+
 
 public class MainApplication extends Application implements ReactApplication {
-
+  private CleverTapAPI mCleverTapInstance;
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
         @Override
@@ -53,7 +62,63 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+
+    //test
+    mCleverTapInstance = CleverTapAPI.getDefaultInstance(this);
+    CleverTapAPI.setDebugLevel(10);
+    initCTGeofenceApi();
+
+
+
   }
+
+  private void initCTGeofenceApi() {
+    if (mCleverTapInstance == null)
+        return;
+
+    // proceed only if cleverTap instance is not null
+    final Context con = this;
+    CTGeofenceAPI.getInstance(getApplicationContext())
+            .init(new CTGeofenceSettings.Builder()
+                    .enableBackgroundLocationUpdates(true)
+                    .setLogLevel(Logger.DEBUG)
+                    .setLocationAccuracy(CTGeofenceSettings.ACCURACY_HIGH)
+                    .setLocationFetchMode(CTGeofenceSettings.FETCH_CURRENT_LOCATION_PERIODIC)
+                    .setGeofenceMonitoringCount(99)
+                    .setInterval(3600000) // 1 hour
+                    .setFastestInterval(1800000) // 30 minutes
+                    .setSmallestDisplacement(1000)// 1 km
+                    .build(), mCleverTapInstance);
+
+    CTGeofenceAPI.getInstance(getApplicationContext())
+            .setOnGeofenceApiInitializedListener(new CTGeofenceAPI.OnGeofenceApiInitializedListener() {
+                @Override
+                public void OnGeofenceApiInitialized() {
+                    Toast.makeText(con, "Geofence API initialized", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    CTGeofenceAPI.getInstance(getApplicationContext())
+            .setCtGeofenceEventsListener(new CTGeofenceEventsListener() {
+                @Override
+                public void onGeofenceEnteredEvent(JSONObject jsonObject) {
+                    Toast.makeText(con, "Geofence Entered", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onGeofenceExitedEvent(JSONObject jsonObject) {
+                    Toast.makeText(con, "Geofence Exited", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    CTGeofenceAPI.getInstance(getApplicationContext())
+            .setCtLocationUpdatesListener(new CTLocationUpdatesListener() {
+                @Override
+                public void onLocationUpdates(Location location) {
+                    Toast.makeText(con, "Location updated", Toast.LENGTH_SHORT).show();
+                }
+            });
+}
 
   /**
    * Loads Flipper in React Native templates. Call this in the onCreate method with something like
